f91b4c105c287a7c983c83edc0e275e2b9b504f4
Imakshat47 <imakshat47@gmail.com>
2021-04-12 08:31:46 +0530

    Update: System Ready!!

--- README.md
+++ README.md
@@ -14,26 +14,59 @@ py main.py
 ## Features
 * Supports Market Order, Limit Order and Stop Order
 * Easy setup by setup walk through:
-    - Un-comment line after #Run next ?
+    - Un-comment line after #Run next ? (ln 117 - 119 @ main.py)
     ```
     _next = input("Do you want to continue? (Y/N): ")
         if(_next == 'N' or _next == 'n'):
             break
     ```
 * To pass data CSV
-    - Un comment and pass user defined file path.
+    - Un comment and pass user defined file path. (ln 48 @ main.py)
     ```
     csv_data = app.extract_csv()
     -or-
     csv_data = app.extract_csv(file_path)
     ```
 
-## Symbols Usage
+## Symbols Usage CSV Format
 
-* bids:
+* bid:
 
-	`B,<symbol>,<exchange>,<id>,<quantity>,<price>,<timestamp>`
+    `B,<symbol>,<exchange>,<quantity>,<price>,<timestamp>,<id>`
 
-* asks:
+* ask:
 
-	`A,<symbol>,<exchange>,<id>,<quantity>,<price>,<timestamp>`
+    `A,<symbol>,<exchange>,<quantity>,<price>,<timestamp>,<id>`
+
+1. Market Order:
+
+    `<type>,<symbol>,<exchange>,<quantity>,<>,<timestamp>,<id>`
+
+2. Limit Order
+
+	`<type>,<symbol>,<exchange>,<quantity>,<price>,<timestamp>,<id>`	
+
+3. Stop Loss
+
+    `<type>,<symbol>,<exchange>,<quantity>,<price>,<timestamp>,<id><trigger_price>`
+
+## Note
+Currently using:
+1. Type 
+* A: Ask or Sell
+* B: Bid or Buy
+
+2. Quantity / Size
+
+3. Price
+
+4. Trigger Price
+
+5. Exchange 
+
+## Output
+![image Output](data/output.png)
+
+## Future Scope
+1. Using Symbol for trading in specific stock.
+2. Using timestamp differentiating for Active Market and after market close.
--- main.py
+++ main.py
@@ -20,100 +20,105 @@ def return_obj(_type, reverse=False):
 
 # driving function
 if __name__ == '__main__':
-    # # Extrating Data from CSV
-    csv_data = [
-        ['B', 'TCS', 'NSE', '700', '11.36', '1618148393', '2'],
-        ['B', 'TCS', 'NSE', '100', '11.35', '1618148394', '1'],
-        ['A', 'TCS', 'NSE', '100', '11.40', '1618148402', '9', '11.39'],
-        ['B', 'TCS', 'NSE', '100', '11.34', '1618148395', '2'],
-        ['B', 'TCS', 'NSE', '100', '', '1618148402', '9'],
-        ['B', 'TCS', 'NSE', '600', '11.33', '1618148396', '3'],
-        ['A', 'TCS', 'NSE', '100', '11.40', '1618148402', '9', '11.38'],
-        ['A', 'TCS', 'NSE', '100', '', '1618148402', '9'],
-        ['A', 'TCS', 'NSE', '100', '11.40', '1618148402', '9', '11.38'],
-        ['B', 'TCS', 'NSE', '700', '11.32', '1618148397', '4'],
-        ['A', 'TCS', 'NSE', '900', '11.42', '1618148398', '5'],
-        ['A', 'TCS', 'NSE', '400', '11.41', '1618148399', '6'],
-        ['A', 'TCS', 'NSE', '205', '11.4', '1618148400', '7'],
-        ['A', 'TCS', 'NSE', '600', '11.39', '1618148401', '8'],
-        ['A', 'TCS', 'NSE', '600', '11.39', '1618148401', '8'],
-        ['A', 'TCS', 'NSE', '400', '11.38', '1618148402', '9'],
-        ['B', 'TCS', 'NSE', '100', '11.38', '1618148402', '9'],
-        ['A', 'TCS', 'NSE', '100', '11.36', '1618148402', '9'],
-        ['A', 'TCS', 'NSE', '100', '', '1618148402', '9'],
-        ['A', 'TCS', 'NSE', '100', '11.36', '1618148402', '9', '11.38'],
-    ]    
-    
-    # csv_data = app.extract_csv()
+    try:
+        # # Extrating Data from CSV
+        csv_data = [
+            ['B', 'TCS', 'NSE', '700', '11.36', '1618148393', '2'],
+            ['B', 'TCS', 'NSE', '100', '11.35', '1618148394', '1'],
+            ['A', 'TCS', 'NSE', '100', '11.40', '1618148402', '9', '11.39'],
+            ['B', 'TCS', 'NSE', '100', '11.34', '1618148395', '2'],
+            ['B', 'TCS', 'NSE', '100', '', '1618148402', '9'],
+            ['B', 'TCS', 'NSE', '600', '11.33', '1618148396', '3'],
+            ['A', 'TCS', 'NSE', '100', '11.40', '1618148402', '9', '11.38'],
+            ['A', 'TCS', 'NSE', '100', '', '1618148402', '9'],
+            ['B', 'TCS', 'NSE', '100', '11.40', '1618148402', '9', '11.38'],
+            ['B', 'TCS', 'NSE', '700', '11.32', '1618148397', '4'],
+            ['A', 'TCS', 'NSE', '900', '11.42', '1618148398', '5'],
+            ['A', 'TCS', 'NSE', '400', '11.41', '1618148399', '6'],
+            ['A', 'TCS', 'NSE', '205', '11.4', '1618148400', '7'],
+            ['A', 'TCS', 'NSE', '600', '11.39', '1618148401', '8'],
+            ['A', 'TCS', 'NSE', '600', '11.39', '1618148401', '8'],
+            ['A', 'TCS', 'NSE', '400', '11.38', '1618148402', '9'],
+            ['B', 'TCS', 'NSE', '100', '11.38', '1618148402', '9'],
+            ['A', 'TCS', 'NSE', '100', '11.36', '1618148402', '9'],
+            ['A', 'TCS', 'NSE', '100', '', '1618148402', '9'],
+            ['B', 'TCS', 'NSE', '100', '11.36', '1618148402', '9', '11.38'],
+        ]
 
-    # Defining Order List Instances
-    _bid = oList.OrderList(True)
-    _ask = oList.OrderList()
-    _sl = oList.OrderList()
+        # csv_data = app.extract_csv()
 
-    # Looping through Queries
-    for data in csv_data:
-        # defining variables
-        _qty = int(data[3])
-        _price = data[4]
+        # Defining Order List Instances
+        _bid = oList.OrderList(True)
+        _ask = oList.OrderList()
+        _sl = oList.OrderList()
 
-        # print("Executing Order => ", data[6])
-        # print("Max Price", _bid._max_price)
-        # print("Max Price", _ask._max_price)
+        # Looping through Queries
+        for data in csv_data:
+            # defining variables
+            _exch = str(data[2])
+            _qty = int(data[3])
+            _price = data[4]
 
-        # Defining Objects
-        _obj = return_obj(data[0])
-        _rev_obj = return_obj(data[0], True)
+            # Defining Objects
+            _obj = return_obj(data[0])
+            _rev_obj = return_obj(data[0], True)
 
-        # For Market Order
-        if(_price == ''):
-            if(_rev_obj._max_price == 0):
-                if _obj == '_bid':
-                    print_msg(
-                        "Market Order can not be Executed. No Sell Order Yet!!")
-                else:
-                    print_msg(
-                        "Market Order can not be Executed. No Buy Order Yet!!")
+            # For Market Order
+            if(_price == ''):
+                if(_rev_obj._max_price == 0):
+                    if _obj == '_bid':
+                        print_msg(
+                            "Market Order can not be Executed. No Sell Order on " + _exch)
+                    else:
+                        print_msg(
+                            "Market Order can not be Executed. No Buy Order on " + _exch)
+                    continue
+                _rev_obj._execute(_qty)
+                print_msg("Market Order Executed @ " + _exch + ". Size = " + str(_qty))
                 continue
-            _rev_obj._execute(_qty)
-            print_msg("Market Order Executed. Size = " + str(_qty))
-            continue
 
-        # For Stop Loss
-        if 0 <= 7 < len(data):
-            market_price = input("Enter the Current the Market Price: ")
-            _sl_price = data[7]
-            if data[0] == 'A':
-                if market_price <= _sl_price <= _price:
-                    # add record to _sl
-                    _sl._add(_sl_price, [_price, _qty])
-            else:
-                if market_price >= _sl_price >= _price:
-                    # add record to _sl
-                    _sl._add(_sl_price, [_price, _qty])
+            # For Stop Loss
+            if 0 <= 7 < len(data):
+                market_price = input("Enter the Current Market Price: ")
+                _sl_price = data[7]
+                if data[0] == 'A':
+                    if market_price <= _sl_price <= _price:
+                        # add record to _sl
+                        _sl._add(_sl_price, [_price, _qty])
+                else:
+                    if market_price >= _sl_price >= _price:
+                        # add record to _sl
+                        _sl._add(_sl_price, [_price, _qty])
+                print_msg(
+                    "Stop Loss Order Added. Order will be added to Order book, once Market @ " + _exch + " hits trigger Price.")
+                continue
 
-        # For Limit Order
-        # Check For limit Order
-        _check = _rev_obj._check(_qty, _price)
-        if(_check == True):
-            print_msg("Limit Order Executed. Size = " + str(_qty))
-            continue
+            # For Limit Order
+            # Check For limit Order
+            _check = _rev_obj._check(_qty, _price)
+            if(_check == True):
+                print_msg("Limit Order Executed @ " + _exch + ". Size = " + str(_qty))
+                continue
 
-        # For un executed Limit order
-        if _check != False:
-            if isinstance(_check, int):
-                _qty = _check
+            # For un executed Limit order
+            if _check != False:
+                if isinstance(_check, int):
+                    _qty = _check
 
-        # Add new entry
-        _obj.add(_qty, _price)
+            # Add new entry
+            _obj.add(_qty, _price)
 
+            # Priting Records
+            app.display_records(_bid._return(), _ask._return())
+            
+            # # Run Next ?
+            # _next = input("Do you want to continue? (Y/N): ")
+            # if(_next == 'N' or _next == 'n'):
+            #     break
+
+        print_msg("All Orders Executed!!")
         # Priting Records
+        print("Final Order Book: ")
         app.display_records(_bid._return(), _ask._return())
-        # # Run Next ?
-        # _next = input("Do you want to continue? (Y/N): ")
-        # if(_next == 'N' or _next == 'n'):
-        #     break
-
-    print_msg("All Orders Executed!!")
-    # Priting Records
-    app.display_records(_bid._return(), _ask._return())
+    except Exception as e:
+        print_msg("Error => " + str(e))
